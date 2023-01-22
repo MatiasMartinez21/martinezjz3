f63406176f2d990ef144a57d357df5104f3779bc
matias <matis01martinez@gmail.com>
2023-01-22 08:35:16 -0300

    segunda ultima

--- main.js
+++ main.js
@@ -69,7 +69,7 @@ if(localStorage.getItem("carrito")){
 
 const contenedorProductos = document.getElementById("contenedorProductos");
 
- 
+ //card productos
 
 const mostrarProductos = () => {
     productos.forEach( producto => {
@@ -86,6 +86,9 @@ const mostrarProductos = () => {
                 </div>`
         contenedorProductos.appendChild(card);
 
+
+
+        
        
         const boton = document.getElementById(`boton${producto.id}`);
         boton.addEventListener("click", () => {
@@ -214,3 +217,16 @@ if(modo === "dark") {
 } else {
     document.body.classList.remove("dark");
 }
+
+
+
+const boton=document.getElementById("boton");
+
+boton.addEventListener("click",()=>{
+  swal.fire({
+    title: "Gracias por tu registro",
+    background: "black",
+    icon:"success"
+    
+    })
+})
